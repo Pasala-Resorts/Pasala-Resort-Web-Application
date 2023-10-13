1d66444963037360c7e7280c9ad4e7c49a907578
himakarCor <himakar@cortracker.com>
2023-10-13 16:22:28 +0530

    10/13/2023

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Switch, useLocation } from 'react-router-dom'; // Import useLocation here
 
 import Header from './components/Header';
 import Gallery from './components/Gallery';
@@ -7,41 +8,84 @@ import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
+import BackendPlugIn from './components/BackendPlugIn';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import BackendPlugIn from './components/BackendPlugIn';
 
-const App = () => (
-  <BrowserRouter>
-    <Header />
-    <BackendPlugIn />
-    <Switch>
-      <Route
-        exact
-        path='/'
-        component={Home}
-      />
-      <Route
-        exact
-        path='/gallery'
-        component={Gallery}
-      />
-      <Route
-        exact
-        path='/about-us'
-        component={AboutUs}
-      />
-      <Route
-        exact
-        path='/contact'
-        component={Contact}
-      />
-    </Switch>
-    <Footer />
-  </BrowserRouter>
-);
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop /> 
+      <Header />
+      <BackendPlugIn />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={Home}
+        />
+        <Route
+          exact
+          path='/gallery'
+          component={Gallery}
+        />
+        <Route
+          exact
+          path='/about-us'
+          component={AboutUs}
+        />
+        <Route
+          exact
+          path='/contact'
+          component={Contact}
+        />
+      </Switch>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+// const App = () => (
+//   <BrowserRouter restoreScroll="auto">
+//     <Header />
+//     <BackendPlugIn />
+//     <Switch>
+//       <Route
+//         exact
+//         path='/'
+//         component={Home}
+//       />
+//       <Route
+//         exact
+//         path='/gallery'
+//         component={Gallery}
+//       />
+//       <Route
+//         exact
+//         path='/about-us'
+//         component={AboutUs}
+//       />
+//       <Route
+//         exact
+//         path='/contact'
+//         component={Contact}
+//       />
+//     </Switch>
+//     <Footer />
+//   </BrowserRouter>
+// );
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,74 +1,58 @@
-import {Link, useHistory} from 'react-router-dom'
-import {Button,Nav,Navbar} from 'react-bootstrap'
 import React, { useState } from 'react';
-import './header.css'
-import 'react-image-gallery/styles/css/image-gallery.css';
-
-
+import { Link, useHistory } from 'react-router-dom';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import './header.css';
 
 function YourNavbarComponent() {
-    const [expanded, setExpanded] = useState(false);
-  
-   
-    const history = useHistory();
-    const handleViewAll = () => {
-          history.push({
-            pathname: '/book-now',
-            state: { bookingPage:true }
-          });
-        }
-    const handleNavbarToggle = () => {
-        setExpanded(!expanded);
-        };
-      
+  const [expanded, setExpanded] = useState(false);
+  const history = useHistory();
 
-    return (
-      <Navbar expand="lg" className="bg-primary-color font-style__Maven-Pro p-3" fixed="top" expanded={expanded}>
-        {/* ... rest of your code ... */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleNavbarToggle}>
-          <i style={{ color: "#fff" }} className="bi-list"></i>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="nav-bar-class navbar-main" onClick={() => setExpanded(false)}>
-            {/* ... your Nav.Link components ... */}
-            <Nav.Link as={Link} to="/" className='font-color'>Home</Nav.Link>
-                <Nav.Link as={Link} to="/gallery" className='font-color'>Gallery</Nav.Link>
-                <Nav.Link as={Link} to="/about-us" className='font-color'>About Us</Nav.Link>
-                <Nav.Link as={Link} to="/Contact" className='font-color'>Contact</Nav.Link>
-                <Nav.Link className='font-color'>
-                <Button onClick={() => {
-              handleViewAll();
-            }} className='btn-color text-dark font-weight-bold'>Book now</Button>
-                </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
-  
+  const handleViewAll = () => {
+    history.push({
+      pathname: '/book-now',
+      state: { bookingPage: true }
+    });
+  };
 
-// class Header extends Component{
-//     render(){
-//         return(
-//             <Navbar  expand="lg" className="bg-primary-color font-style__Maven-Pro p-3" fixed="top">
-//                 <Container className='navbar-main'>
-//                     <Navbar.Brand href="/"><img src='https://res.cloudinary.com/dobpvkjao/image/upload/v1693213313/logo_1_lszeve.png' alt="logo" className='img-logo'/></Navbar.Brand>
-//                     <Navbar.Toggle aria-controls="basic-navbar-nav"><i style={{color:"#fff"}} class="bi-list"></i></Navbar.Toggle>
-//                     <Navbar.Collapse id="basic-navbar-nav">
-//                     <Nav className="nav-bar-class  navbar-main">
-//                         <Nav.Link as={Link} to="/" className='font-color'>Home</Nav.Link>
-//                         <Nav.Link as={Link} to="/gallery" className='font-color'>Gallery</Nav.Link>
-//                         <Nav.Link as={Link} to="/about-us" className='font-color'>About Us</Nav.Link>
-//                         <Nav.Link as={Link} to="/Contact" className='font-color'>Contact</Nav.Link>
-//                         <Nav.Link as={Link} to="/book-now" className='font-color'><Button className='btn-color text-dark font-weight-bold'>Book now</Button></Nav.Link>
-//                             {/* <a href='https://77409.staygrid.com/webreservation/index/index/hidHotelIdWebOut/QV5TX0ZSczM0Xzc3NDA5X0Y1dGVyOTA4N3NfKWRoZl9kcnRlcjdfNzc0MDlfaGdmaF9nXmQ4NTQ/hidLanguageId/1/'><Button className='btn-color text-dark font-weight-bold'>Book now</Button></a> */}
-//                         {/* <div id="webDiv" style={{borderWidth: 0}}></div> */}
-//                     </Nav>
-//                     </Navbar.Collapse>
-//                 </Container>
-//             </Navbar>
-//         )
-//     }
-// }
+  const handleNavbarToggle = () => {
+    setExpanded(!expanded);
+  };
 
-export default YourNavbarComponent
+  return (
+    <Navbar
+      expand="lg"
+      className="bg-primary-color font-style__Maven-Pro p-3"
+      fixed="top"
+      expanded={expanded}
+    >
+      {/* ... rest of your code ... */}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleNavbarToggle}>
+        <i style={{ color: '#fff' }} className="bi-list"></i>
+      </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="nav-bar-class navbar-main" onClick={() => setExpanded(false)}>
+          {/* ... your Nav.Link components ... */}
+          <Nav.Link as={Link} to="/" className="font-color">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/gallery" className="font-color">
+            Gallery
+          </Nav.Link>
+          <Nav.Link as={Link} to="/about-us" className="font-color">
+            About Us
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" className="font-color">
+            Contact
+          </Nav.Link>
+          <Nav.Link className="font-color">
+            <Button onClick={handleViewAll} className="btn-color text-dark font-weight-bold">
+              Book now
+            </Button>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default YourNavbarComponent;
